1c6ff9bbe0c2f5339eef3e87c0dc5e811ec776c8
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-02-19 17:40:44 -0600

    created test for the full constructor to make sure that it assigns class an value for each field

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -3,6 +3,9 @@ package org.launchcode.techjobs_oo.Tests;
 import org.junit.Before;
 import org.junit.Test;
 import org.launchcode.techjobs_oo.*;
+
+import java.time.LocalDate;
+
 import static org.junit.Assert.*;
 
 public class JobTest {
@@ -23,5 +26,21 @@ public class JobTest {
     public void testSettingJobId(){
         assertNotEquals(test_job1, test_job2);
         assertEquals(1, test_job1.getId() - test_job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job productTester = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertEquals("Product tester", productTester.getName());
+        assertEquals("ACME", productTester.getEmployer().getValue());
+        assertEquals("Desert", productTester.getLocation().getValue());
+        assertEquals("Quality control", productTester.getPositionType().getValue());
+        assertEquals("Persistence", productTester.getCoreCompetency().getValue());
+
+        assertNotNull(productTester.getEmployer());
+        assertNotNull(productTester.getLocation());
+        assertNotNull(productTester.getPositionType());
+        assertNotNull(productTester.getCoreCompetency());
+
     }
 }
